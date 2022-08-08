7c40bdf74d8ddaea38c70484fde741357e702d5e
Henrik Sedvall <henrik.sedvall@gmail.com>
2022-08-08 14:39:06 +0200

    Initial commit

---
+++ hs_website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
